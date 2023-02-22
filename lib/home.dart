f537be127a7c3e3570a88f6f4ec8f16b192963e2
Sakthivel K <sakthivel.karunanithi@augustahitech.com>
2023-02-22 15:41:48 +0530

    bottom navigation added

---
+++ lib/home.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'constant.dart';
+
+void main() {
+  runApp(new MaterialApp(
+    home: new MyApp(),
+  ));
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  final tabs = [
+    Center(child: Text("Welcome"),),
+    Center(child: Text("Search"),),
+    Center(child: Text("Notification"),),
+    Center(child: Text("Settings"),),
+    Center(child: Text("Profile"),),
+  ];
+  int index = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        title: Text(
+          title,
+          style: TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold),
+        ),
+        automaticallyImplyLeading: false,
+      ),
+      body: tabs[index],
+      bottomNavigationBar: BottomNavigationBar(
+
+        currentIndex: index,
+        items: [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              backgroundColor: Colors.red,
+              label: "Home"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.search),
+              backgroundColor: Colors.red,
+              label: "Search"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.notifications),
+              backgroundColor: Colors.red,
+              label: "NOtification"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.settings),
+              backgroundColor: Colors.red,
+              label: "Settings"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              backgroundColor: Colors.red,
+              label: "Profile"),
+        ],
+        onTap: (current_index) {
+          setState(() {
+            index = current_index;
+          });
+        },
+      ),
+    );
+  }
+}
--- lib/login.dart
+++ lib/login.dart
@@ -1,138 +1,145 @@
 import "package:flutter/material.dart";
 import 'constant.dart';
+import 'home.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    double height = MediaQuery
-        .of(context)
-        .size
-        .height;
-    double width = MediaQuery
-        .of(context)
-        .size
-        .width;
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: SingleChildScrollView(
-        child:Form(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(
-                height: 50,
-              ),
-              Image.asset(
-                bgimage,
-                height: height * 0.32,
-                width: width,
-                fit: BoxFit.cover,
-              ),
-              SizedBox(
-                height: 30,
-              ),
-              Center(
-                child: Text('Login',
-                    style: new TextStyle(
-                        fontSize: 30.0,
-                        fontWeight: FontWeight.bold,
-                        foreground: Paint()
-                          ..shader = LinearGradient(
-                            colors: <Color>[
-                              Colors.black,
-                              Colors.redAccent,
-                            ],
-                          ).createShader(Rect.fromLTWH(0.0, 0.0, 200.0, 100.0)))),
-              ),
-              SizedBox(height: 30),
-              Padding(
-                padding: const EdgeInsets.only(left: 20.0, right: 20.0),
-                child: TextFormField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.black)),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.black),
+    double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        body: SingleChildScrollView(
+          child: Form(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  height: 50,
+                ),
+                Image.asset(
+                  bgimage,
+                  height: height * 0.32,
+                  width: width,
+                  fit: BoxFit.cover,
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                Center(
+                  child: Text('Login',
+                      style: new TextStyle(
+                          fontSize: 30.0,
+                          fontWeight: FontWeight.bold,
+                          foreground: Paint()
+                            ..shader = LinearGradient(
+                              colors: <Color>[
+                                Colors.black,
+                                Colors.redAccent,
+                              ],
+                            ).createShader(
+                                Rect.fromLTWH(0.0, 0.0, 200.0, 100.0)))),
+                ),
+                SizedBox(height: 30),
+                Padding(
+                  padding: const EdgeInsets.only(left: 20.0, right: 20.0),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.black)),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.black),
+                      ),
+                      prefixIcon: Icon(Icons.email, color: Colors.red),
+                      labelText: "Email Address",
+                      labelStyle:
+                          TextStyle(color: Colors.black, fontSize: 13.0),
                     ),
-                    prefixIcon: Icon(Icons.email, color: Colors.red),
-                    labelText: "Email Address",
-                    labelStyle: TextStyle(color: Colors.black,fontSize:13.0),
                   ),
                 ),
-              ),
-              SizedBox(height: 30),
-              Padding(
-                padding: const EdgeInsets.only(left: 20.0, right: 20.0),
-                child: TextFormField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.black)),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.black),
+                SizedBox(height: 30),
+                Padding(
+                  padding: const EdgeInsets.only(left: 20.0, right: 20.0),
+                  child: TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.black)),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.black),
+                      ),
+                      prefixIcon: Icon(Icons.lock_open, color: Colors.red),
+                      labelText: "Password",
+                      labelStyle:
+                          TextStyle(color: Colors.black, fontSize: 13.0),
                     ),
-                    prefixIcon: Icon(Icons.lock_open, color: Colors.red),
-                    labelText: "Password",
-                    labelStyle: TextStyle(color: Colors.black,fontSize:13.0),
                   ),
                 ),
-              ),
-              Align(
-                alignment: Alignment.centerRight,
-                child: TextButton(
-                  onPressed: () {},
-                  child: Text(
-                    "Forget Password?",
-                    style: TextStyle(color: Colors.red),
-                  ),
-                ),
-              ),
-              SizedBox(height: 20),
-              Center(
-                child: SizedBox(
-                  height: height * 0.08,
-                  width: width - 30,
+                Align(
+                  alignment: Alignment.centerRight,
                   child: TextButton(
                     onPressed: () {},
-                    style: TextButton.styleFrom(
-                        backgroundColor: Colors.red,
-                        shadowColor: Colors.black,
-                        elevation: 5,
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.all(Radius.circular(10)))),
                     child: Text(
-                      "Login to account",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 20.0,
-                        fontWeight: FontWeight.bold,
+                      "Forget Password?",
+                      style: TextStyle(color: Colors.red),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20),
+                Center(
+                  child: SizedBox(
+                    height: height * 0.08,
+                    width: width - 30,
+                    child: TextButton(
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => MyApp())
+                        );
+                      },
+                      style: TextButton.styleFrom(
+                          backgroundColor: Colors.red,
+                          shadowColor: Colors.black,
+                          elevation: 5,
+                          shape: RoundedRectangleBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(10)))),
+                      child: Text(
+                        "Login to account",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
                     ),
                   ),
                 ),
-              ),
-              SizedBox(
-                height: 20.0,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text("Don't have an account?"),
-                  TextButton(
-                      onPressed: () {},
-                      child: Text(
-                        "Create an account",
-                        style: TextStyle(color: Colors.red),
-                      ))
-                ],
-              )
-            ],
+                SizedBox(
+                  height: 20.0,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("Don't have an account?"),
+                    TextButton(
+                        onPressed: () {},
+                        child: Text(
+                          "Create an account",
+                          style: TextStyle(color: Colors.red),
+                        ))
+                  ],
+                )
+              ],
+            ),
           ),
         ),
       ),
     );
   }
- }
+}
